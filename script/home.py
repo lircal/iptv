50c2b657843351c728c48d348ba360b4b226164f
lin <lin@Debian.lan>
2025-09-24 21:50:35 +0800

    Change the host from 192.168.6.1:10100 to 192.168.6.3:10337

--- script/home.py
+++ script/home.py
@@ -16,7 +16,7 @@ import re
 sourceTvboxIptv="https://raw.githubusercontent.com/gaotianliuyun/gao/master/list.txt"
 sourceIcon51ZMT="http://epg.51zmt.top:8000"
 sourceChengduMulticast="http://epg.51zmt.top:8000/sctvmulticast.html"
-homeLanAddress="http://192.168.6.1:10100"
+homeLanAddress="http://192.168.6.3:10337"
 
 groupCCTV=["CCTV", "CETV", "CGTN"]
 groupWS=[ "卫视"]
